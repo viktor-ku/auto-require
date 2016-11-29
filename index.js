5e50b979bdcbe9137e99f1cd160c83b26c397259
ViktorKonsta <viktor.konsta@gmail.com>
2016-11-29 14:56:13 +0200

    make for each function it's own file and reorganize index file

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-module.exports = function (options = {}) {
-  return require('./reqr').init(options)
+const parseOptions = require('./lib/parseOptions')
+const parseModulesMap = require('./lib/parseModulesMap')
+const makePackageNames = require('./lib/makePackageNames')
+const requireModules = require('./lib/requireModules')
+const zipObject = require('./lib/zipObject')
+
+module.exports = function autoRequire (options) {
+  options = parseOptions(options)
+  const schema = options.schema
+  const globaly = options.globaly
+  const map = parseModulesMap(schema)
+  const packageNames = makePackageNames(map.packages)
+  const modules = requireModules(map.fullPaths)
+  const collection = zipObject(packageNames, modules)
+  if (globaly) {
+    for (const k in collection) {
+      global[k] = collection[k]
+    }
+  }
+  return collection
 }
