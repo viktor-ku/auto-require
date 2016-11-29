b77ee31e2472d9d60d2eddcd235d929e7ec1c88e
ViktorKonsta <viktor.konsta@gmail.com>
2016-11-29 16:31:49 +0200

    Add some tests to autoRequire global function

--- test/autoRequire.js
+++ test/autoRequire.js
@@ -2,8 +2,14 @@ const t = require('tap')
 const autoRequire = require('../index')
 
 t.test('autoRequire', t => {
-  const collection = autoRequire()
+  const collection = autoRequire({
+    globaly: true
+  })
   t.ok(collection)
+  t.ok(collection.lodashCamelcase)
+  t.ok(collection.lodashZipobject)
+  t.ok(collection.eslint)
+  t.deepEqual(collection.eslint, global.eslint)
   t.ok(Object.keys(collection).length)
   t.end()
 })
