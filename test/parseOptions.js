95934b1f9a2990a7f3b2eb77e23a6b3dae2e110b
ViktorKonsta <viktor.konsta@gmail.com>
2016-11-29 16:14:32 +0200

    test parseOptions

---
+++ test/parseOptions.js
@@ -0,0 +1,20 @@
+const t = require('tap')
+const parseOptions = require('../lib/parseOptions')
+
+t.test('parseOptions', t => {
+  const options = parseOptions({
+    without: ['eslint-config-standard', 'eslint-plugin-promise', 'eslint-plugin-standard']
+  })
+  t.equal(options.globaly, false, 'globaly should be set to false by default')
+  t.ok(options.schema)
+  t.ok(options.schema['node_modules'])
+  t.equal(Array.isArray(options.schema['node_modules']), true)
+  t.equal(options.schema['node_modules'].includes('eslint'), true)
+  t.equal(options.schema['node_modules'].includes('tap'), true)
+  t.equal(options.schema['node_modules'].includes('lodash.camelcase'), true)
+  t.equal(options.schema['node_modules'].includes('lodash.zipobject'), true)
+  t.equal(options.schema['node_modules'].includes('eslint-config-standard'), false)
+  t.equal(options.schema['node_modules'].includes('eslint-plugin-promise'), false)
+  t.equal(options.schema['node_modules'].includes('eslint-plugin-standard'), false)
+  t.end()
+})
