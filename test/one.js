38096e99d938859c08c5142318c5a220a98ecff2
ViktorKonsta <viktor.konsta@gmail.com>
2016-11-29 15:33:14 +0200

    Remove very small and relatively useless one.js function

--- test/one.js
+++
@@ -1,8 +0,0 @@
-const t = require('tap')
-const one = require('../lib/one')
-
-t.test('one', t => {
-  t.equal(one(['first element']), true, 'one is true when array length is 1')
-  t.equal(one(['first element', 'second element']), false, 'one is false when array length is not 1')
-  t.end()
-})
